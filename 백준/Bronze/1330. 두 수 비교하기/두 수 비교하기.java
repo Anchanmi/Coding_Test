8849931163e80595f6119714debf15d9c46a8b74
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-07-20 16:11:39 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 220 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a >= -10000 && a <= 10000 && b>= -10000 && b <= 10000) {
+			if(a > b) {
+				System.out.println(">");
+			}
+			else if(a < b){
+				System.out.println("<");
+			}
+			else if(a == b) {
+				System.out.println("==");
+			}
+		}
+		
+		sc.close();
+	}
+
+}
