ce6f3b415cec354af93b93199177d0c01581f9ba
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-07-20 16:07:18 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if(a>0 && a<10 && b>0 && b<10) {
+			System.out.println(a + b);
+		}
+		
+	}
+
+}
