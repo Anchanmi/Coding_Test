bd5514b5169be19710b4592bde22d48e31be09f6
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-07-20 17:05:14 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder str = new StringBuilder();
+		ArrayList<String> list = new ArrayList<>();
+		
+		long T = Long.parseLong(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a > 0 && a < 10 && b > 0 && b < 10) {
+				int result = a + b;
+				list.add("Case #" + (i + 1) + ": " + result);
+			}
+		}
+		
+		for(int i  = 0; i < list.size(); i++) {
+			str.append(list.get(i) + "\n");
+		}
+		
+		System.out.println(str);
+		
+	}
+
+}
