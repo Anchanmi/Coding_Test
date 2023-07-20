b0520fe28f93cdbb21c796943be030efdfe68298
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-07-20 16:10:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=1000 && a<=3000){
+            System.out.println(a-543);
+        }
+        sc.close();
+    }
+}
