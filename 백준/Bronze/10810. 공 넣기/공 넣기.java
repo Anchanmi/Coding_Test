0f302dd389dff5f31332faa9add792572b943a0c
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-07-26 17:05:30 +0900

    [Bronze III] Title: 공 넣기, Time: 152 ms, Memory: 15824 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder str = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			for(int j = a; j <= b; j++) {
+				arr[j-1] = k;
+			}
+		}
+		
+		for(int i : arr) {
+			str.append(i + " ");
+		}
+		System.out.println(str);
+		
+	}
+
+}
