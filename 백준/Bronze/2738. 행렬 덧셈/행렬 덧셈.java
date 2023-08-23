81563a1cab82670f842ac7f751e47c06b3324488
Chandomi <103572952+Anchanmi@users.noreply.github.com>
2023-08-23 14:21:12 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 232 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,45 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] arr1 = new int[N][M];
+		int[][] arr2 = new int[N][M];
+		int[][] result = new int[N][M];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				result[i][j] = arr1[i][j] +  arr2[i][j];
+				sb.append(result[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
